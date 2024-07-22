42d7fced343b06eb2b921c4a92b1b89e2b24f783
anthonyshibitov <anthonyshibitov@gmail.com>
2024-07-21 20:36:34 -0400

    Preliminary front end scaffolding

--- backend/netdis/core/urls.py
+++ backend/netdis/core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import test_view, binary_ingest, probe, cfg, funcs, blocks, disasms
+from .views import test_view, binary_ingest, probe, funcs, blocks, disasms
 
 urlpatterns = [
     path('test/', test_view, name='test'),
     path('binary_ingest/', binary_ingest, name='binary_ingest'),
     path('probe/', probe, name='probe'),
-    path('cfg/', cfg, name='cfg'),
     
     path('funcs/', funcs, name='funcs'),
     path('blocks/', blocks, name='funcs'),
--- backend/netdis/core/views.py
+++ backend/netdis/core/views.py
@@ -6,7 +6,6 @@ from rest_framework import status
 from django.http import Http404, HttpResponseBadRequest
 from .models import UploadedFile, Project, Function, Block, Disasm
 import hashlib
-import angr
 import json
 import networkx as nx
 from .utils import get_project_from_hash, get_functions_from_project, get_blocks_from_function, get_disasm_from_block, analyze_file
@@ -18,6 +17,7 @@ def test_view(request):
     data = {"result": "test"}
     return Response(data)
 
+@timer
 @api_view(['POST'])
 @parser_classes([MultiPartParser])
 def binary_ingest(request):
@@ -27,113 +27,40 @@ def binary_ingest(request):
         hash = hashlib.sha256(contents).hexdigest()
         file_obj.name = hash
                 
+        # Uploaded file, and analysis already exists
         if UploadedFile.objects.filter(hash = hash).exists():
             uploaded_file = UploadedFile.objects.get(hash = hash)
-            print("already exists")
+        # Uploaded file does not exist. Upload, analyze, and delete it.
         else:
-            print("new file!")
-            new_file = UploadedFile(file=file_obj, hash=hash)
-            new_file.save()
-            # DO ANALYSIS AND DB SAVING
-            uploaded_file = new_file
+            uploaded_file = UploadedFile(file=file_obj, hash=hash)
+            uploaded_file.save()
             analyze_file(uploaded_file)
             uploaded_file.delete()
  
-        project = Project.objects.get(file = uploaded_file)
-        print(f"We have project id {project.id}")
-            
-        return Response(project.id)
+        # Return project ID for file
+        project = Project.objects.get(file = uploaded_file)            
+        # return Response(project.id)
+        return Response(hash)
     return Response("Bad request!", status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['POST'])
-@timer
-def cfg(request):
-    if(request.body):
-        data_dict = json.loads(request.body.decode("utf-8"))
-        hash = data_dict['hash']
-        proj = get_project_from_hash(hash)
-        funcs = get_functions_from_project(proj)
-        funcs_list = list()
-        for func in funcs:
-            funcs_list.append(func.name)
-        #return Response(funcs_list)
-        
-        if UploadedFile.objects.filter(hash = hash).exists():
-            file = UploadedFile.objects.filter(hash = hash).first()
-            file_path = "./media/" + file.file.name
-            proj = angr.Project(file_path, load_options={'auto_load_libs': False})
-            
-            # Check if project already exists with this hash
-            if(Project.objects.filter(file=file)).exists():
-                proj_obj = Project.objects.get(file=file)
-                return Response(proj_obj.id)
-            
-            proj_obj = Project(file = file)
-            proj_obj.save()
-            print(f"Project ID loaded: {proj_obj.id}")
-            cfg = proj.analyses.CFGFast()
-            
-            
-            graph = nx.DiGraph()
-            dis_list = list()
-            for node in cfg.graph.nodes():
-                graph.add_node(node)
-
-            for edge in cfg.graph.edges():
-                graph.add_edge(edge[0], edge[1])
-                
-            cfg_data = {
-                'functions': {}
-            }
-            
-            for func in cfg.kb.functions.values():
-                function_data = {
-                    'name': func.name,
-                    'blocks': {}
-                }
-                
-                function_obj = Function(project = proj_obj, name = func.name, addr = hex(func.addr))
-                function_obj.save()
-                
-                for block in func.blocks:
-                    block_data = {
-                        'addr': hex(block.addr),
-                        'disas': None
-                    }
-                    
-                    cb = proj.factory.block(block.addr).capstone
-                    block_data['disas'] = [
-                        {'addr': hex(insn.address), 'op': insn.mnemonic, 'data': insn.op_str}
-                        for insn in cb.insns
-                    ]
-                    
-                    block_obj = Block(function = function_obj, addr = hex(block.addr))
-                    block_obj.save()
-                    
-                    for insn in cb.insns:
-                        disasm_obj = Disasm(block = block_obj, op = insn.mnemonic, data = insn.op_str, addr = hex(insn.address))
-                        disasm_obj.save()
-
-                    function_data['blocks'][hex(block.addr)] = block_data
-                    
-                cfg_data['functions'][hex(func.addr)] = function_data
-            
-            return Response(cfg_data)
-                
-    return Response("error")
 
 @api_view(['POST'])
 def funcs(request):
     if(request.body):
         data_dict = json.loads(request.body.decode("utf-8"))
-        project_id = data_dict['project_id']
+        try:
+            project_id = data_dict['project_id']
+        except Exception as error:
+            return Response(f"ERROR: {error.__str__()}")
         return Response(get_functions_from_project(project_id))
     return Response('BAD')
 
 @api_view(['POST'])
 def blocks(request):
     if(request.body):
-        data_dict = json.loads(request.body.decode("utf-8"))
+        try:
+            data_dict = json.loads(request.body.decode("utf-8"))
+        except Exception as error:
+            return Response(error)
         function_id = data_dict['function_id']
         return Response(get_blocks_from_function(function_id))
     return Response('BAD')
@@ -151,3 +78,15 @@ def disasms(request):
 def probe(request):
     print(request.user.id)
     return Response(request.user.id)
+
+def test_cfg_func(request):
+    pass
+    # cfg = proj.analyses.CFGFast()
+    
+    # graph = nx.DiGraph()
+    # dis_list = list()
+    # for node in cfg.graph.nodes():
+    #     graph.add_node(node)
+
+    # for edge in cfg.graph.edges():
+    #     graph.add_edge(edge[0], edge[1])
