c173fa0afd2271a001f76bfa45febd956691b56e
anthonyshibitov <anthonyshibitov@gmail.com>
2024-08-02 14:09:57 -0400

    Add tailwindcss
    I don't like writing CSS and I'm bad at it. Starting to transition
    to using only tailwindcss.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
-import './HomePage.css';
 import NavBar from '../components/NavBar';
 
 export default function HomePage() {
     console.log(import.meta.env.VITE_BACKEND);
     return (
-        <div className="main-container">
-            <NavBar />
-            <div className="logo">
-            <span className="blue">net</span><span className="grey">dis</span>
+        <div>
+            <div className="flex gap-4">
+                <NavBar />
+            </div>
+            <div className="logo-placeholder-css">
+                <span className="text-ndblue">net</span><span className="text-ndgrey">dis</span>
             </div>
         </div>
     )
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'ndblue': '#5b92e5',
+        'ndgrey': '#2a2b2a',
+      }
+    }
+  },
+  plugins: [],
+}
