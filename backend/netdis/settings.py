6e4b0ee6d78e295fb0b826779534a7540b92167f
anthonyshibitov <anthonyshibitov@gmail.com>
2024-08-17 16:46:55 -0400

    Fix debug settings

--- backend/netdis/settings.py
+++ backend/netdis/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = int(os.environ.get('DEBUG', 0)) 
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
