fa853512c13774eae023c7b039cd5f775cb416fb
anthonyshibitov <anthonyshibitov@gmail.com>
2024-08-26 20:21:36 -0400

    Change nginx max body size

--- frontend/src/components/RawHex.jsx
+++ frontend/src/components/RawHex.jsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 
 export default function RawHex() {
     const [address, setAddress] = useState();
+
     function handleChange(event){
         const newAddress = event.target.value;
         setAddress(newAddress);
     }
 
+    function sendAddressRequest(){
+        
+    }
+
     return (
         <div className="text-xs font-mono">
             <input className="border border-black" type="text" onChange={handleChange} />
-            <button className="px-1 border-2 bg-ndblue text-white">View</button>
+            <button className="px-1 border-2 bg-ndblue text-white" onClick={sendAddressRequest}>View</button>
             {address}
         </div>
     )
